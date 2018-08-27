9462bff206a390d9f0b67baacddd60b38a0ab6e8
Abhijay Saran <git@abhijay.com>
2018-08-27 08:48:32 -0700

    Finished Simple Nim

--- wk1-counting-game/nim.ts
+++ wk1-counting-game/nim.ts
@@ -2,23 +2,36 @@
 // Author a recursive backtracking solver in Python
 // for the Nim-like game 10, 9, … 1 (starting with 10 coins, remove one or two on your turn
 // with the goal of removing the last coin) and rudimentary playing system
+
+// If you are left to draw from...
+
+declare var require: any
+declare var process: any
+
+
 // const BAD_MOVE = -1;
+
 // const maxCoins = 10;
 const maxDraw = 2;
 // const players = ['HUMAN', 'COMPUTER']
+
 // let totalCoins = maxCoins;
 // let currTurn = players[0];
+
 // function switchTurn() {
 //     currTurn = players.find(p => p != currTurn);
 // }
+
 // function playGame(num, currCoins) {
 //     if (currCoins === 0) return currCoins;
+
 //     if (currTurn === 'HUMAN') {
 //         return makeMove(num, currCoins)
 //     } else {
 //         // return makeComputer
 //     }
 // }
+
 // function makeMove(num, currCoins) {
 //     // console.log(currTurn)
 //     if (num > maxDraw) {
@@ -26,29 +39,37 @@ const maxDraw = 2;
 //     } else if (num < maxDraw) {
 //         num = 1;
 //     }
+
 //     if (currCoins - num > 0) {
 //         currCoins -= num;
 //         switchTurn();
 //     } else {
 //         currCoins = 0
 //     }
+
 //     return currCoins;
 // }
+
 // function determineGoodMove(currCoins) {
 //     let limit = (currCoins < maxDraw) ? currCoins : maxDraw;
+
 //     for (let nTaken = 1; nTaken <= limit; nTaken++) {
 //         if (determineBadPosition(currCoins - nTaken)) {
 //             return nTaken;
 //         }
 //     }
+
 //     return BAD_MOVE;
 // }
+
 // function determineBadPosition(currCoins) {
 //     if (currCoins % 2 == 1) return true;
 //     if (currCoins == 0) return false;
 //     return determineGoodMove(currCoins) == BAD_MOVE;
 // }
+
 // determineGoodMove(5)
+
 // const stdin = process.openStdin();
 // var readline = require('readline');
 // var rl = readline.createInterface(process.stdin, process.stdout);
@@ -57,17 +78,20 @@ const maxDraw = 2;
 // rl.on('line', function (line) {
 //     if (Number.isSafeInteger(Number(line))) {
 //         totalCoins = makeMove(line, totalCoins);
+
 //         if (totalCoins !== 0) {
 //             let computerMove = determineGoodMove(totalCoins);
 //             if (totalCoins - computerMove == 0) {
 //                 rl.close();
 //             } else {
 //                 if (computerMove == -1) computerMove = 1;
+
 //                 switchTurn()
 //                 rl.setPrompt([
 //                     `with ${totalCoins} left, COMPUTER made ${computerMove}`,
 //                     `${currTurn} draw 1 or 2 coin(s) (${totalCoins - computerMove} coins left)> `
 //                 ].join('\n'));
+
 //                 totalCoins -= computerMove;
 //             }
 //         } else {
@@ -79,18 +103,21 @@ const maxDraw = 2;
 //     console.log(`winner: ${currTurn}`)
 //     process.exit(0);
 // });
-var SpecialMoves;
-(function (SpecialMoves) {
-    SpecialMoves["LOSE_MOVE"] = "Lose";
-    SpecialMoves["UNDECIDED_MOVE"] = "Undecided";
-    SpecialMoves["WIN_MOVE"] = "Win";
-})(SpecialMoves || (SpecialMoves = {}));
+
+enum SpecialMoves {
+    LOSE_MOVE = "Lose",
+    UNDECIDED_MOVE = "Undecided",
+    WIN_MOVE = "Win"
+}
+
 function doMove(position, move) {
     return position - move;
 }
+
 function generateMoves(position) {
     return (position < maxDraw) ? position : position % 3;
 }
+
 function primitive(position) {
     if (position % 3 == 0) {
         return SpecialMoves.LOSE_MOVE;
@@ -100,13 +127,14 @@ function primitive(position) {
     }
     else {
         // return SpecialMoves.UNDECIDED_MOVE;
-        return primitive(position - doMove(position, generateMoves(position)));
+        return primitive(position - doMove(position, generateMoves(position)))
     }
 }
+
 function solve(position) {
     return primitive(position);
 }
+
 for (let i of [...Array(11).keys()]) {
     console.log(solve(i));
 }
-//# sourceMappingURL=nim.js.map
