68f0e9c56afdd5574e673362b3dc443302c4e33a
Abhijay Saran <git@abhijay.com>
2018-11-28 17:42:43 -0800

    hard stash

--- wk1-counting-game/nim.js
+++ wk1-counting-game/nim.js
@@ -2,34 +2,51 @@
 // Author a recursive backtracking solver in Python
 // for the Nim-like game 10, 9, … 1 (starting with 10 coins, remove one or two on your turn
 // with the goal of removing the last coin) and rudimentary playing system
-const maxDraw = 2;
 var SpecialMoves;
 (function (SpecialMoves) {
     SpecialMoves["LOSE_MOVE"] = "Lose";
     SpecialMoves["UNDECIDED_MOVE"] = "Undecided";
     SpecialMoves["WIN_MOVE"] = "Win";
 })(SpecialMoves || (SpecialMoves = {}));
+const memoized = new Map();
+memoized.set(0, SpecialMoves.LOSE_MOVE);
+memoized.set(1, SpecialMoves.WIN_MOVE);
+memoized.set(2, SpecialMoves.WIN_MOVE);
 function doMove(position, move) {
-    return position - move;
+    return position + move;
 }
 function generateMoves(position) {
-    return (position < maxDraw) ? position : position % 3;
+    return (position < 2) ? [1] : (position < 3) ? [1, 2] : [1, 2, 3];
 }
 function primitive(position) {
-    if (position % 3 == 0) {
+    if (position == 20) {
         return SpecialMoves.LOSE_MOVE;
     }
-    else if (position - maxDraw <= maxDraw) {
+    else if (position == 19 || position == 18) {
         return SpecialMoves.WIN_MOVE;
     }
     else {
-        return primitive(position - doMove(position, generateMoves(position)));
+        return SpecialMoves.UNDECIDED_MOVE;
     }
 }
 function solve(position) {
-    return primitive(position);
+    if (memoized.get(position)) {
+        return memoized.get(position);
+    };
+
+    if (primitive(position) != SpecialMoves.UNDECIDED_MOVE)
+        return primitive(position);
+    for (let move of generateMoves(position)) {
+        let newPosition = doMove(position, move);
+        if (solve(newPosition) == SpecialMoves.LOSE_MOVE) {
+            memoized.set(position, SpecialMoves.WIN_MOVE);
+            return SpecialMoves.WIN_MOVE;
+        }
+    }
+    memoized.set(position, SpecialMoves.LOSE_MOVE);
+    return SpecialMoves.LOSE_MOVE;
 }
-for (let i of [...Array(11).keys()]) {
-    console.log(solve(i));
+for (const i of [...Array(21).keys()]) {
+    console.log(`position ${i}: ${solve(i)}`);
 }
 //# sourceMappingURL=nim.js.map
--- wk1-counting-game/nim.ts
+++ wk1-counting-game/nim.ts
@@ -6,38 +6,49 @@
 declare var require: any
 declare var process: any
 
-const maxDraw = 2;
-
 enum SpecialMoves {
     LOSE_MOVE = "Lose",
     UNDECIDED_MOVE = "Undecided",
     WIN_MOVE = "Win"
 }
 
+const memoized = new Map();
+memoized.set(0, SpecialMoves.LOSE_MOVE)
+memoized.set(1, SpecialMoves.WIN_MOVE)
+memoized.set(2, SpecialMoves.WIN_MOVE)
+
 function doMove(position, move) {
     return position - move;
 }
-
 function generateMoves(position) {
-    return (position < maxDraw) ? position : position % 3;
+    return (position < 2) ? [1] : [1,2];
 }
-
 function primitive(position) {
-    if (position % 3 == 0) {
-        return SpecialMoves.LOSE_MOVE;
-    }
-    else if (position - maxDraw <= maxDraw) {
+    if (position == 0) {
+        return SpecialMoves.LOSE_MOVE
+    } else if (position == 1 || position == 2) {
         return SpecialMoves.WIN_MOVE;
     }
     else {
-        return primitive(position - doMove(position, generateMoves(position)))
+        return SpecialMoves.UNDECIDED_MOVE;
     }
 }
-
 function solve(position) {
-    return primitive(position);
+    if (memoized.get(position)) { return memoized.get(position) };
+    if (primitive(position) != SpecialMoves.UNDECIDED_MOVE) return primitive(position);
+
+    for (let move of generateMoves(position)) {
+        let newPosition = doMove(position, move);
+        if (solve(newPosition) == SpecialMoves.LOSE_MOVE) {
+            memoized.set(position, SpecialMoves.WIN_MOVE)
+            return SpecialMoves.WIN_MOVE;
+        }
+    }
+
+    memoized.set(position, SpecialMoves.LOSE_MOVE)
+    return SpecialMoves.LOSE_MOVE
 }
 
-for (let i of [...Array(11).keys()]) {
-    console.log(solve(i));
+for (const i of [...Array(100).keys()]) {
+   console.log(solve(100-i));
 }
